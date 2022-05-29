17fc4ff14f95e595d4749f93e2a58ee6d4b00c4d
Annan Wang <annan.wang@shopee.com>
2022-05-30 00:48:20 +0800

    debug]

--- trainers/cocoop.py
+++ trainers/cocoop.py
@@ -149,6 +149,7 @@ class PromptLearner(nn.Module):
         bias = bias.unsqueeze(1)           # (batch, 1, ctx_dim)
         ctx = ctx.unsqueeze(0)             # (1, n_ctx, ctx_dim)
         ctx_shifted = ctx + bias           # (batch, n_ctx, ctx_dim)
+        print(ctx_shifted.seiz())
         
         # Use instance-conditioned context tokens for all classes
         prompts = []
