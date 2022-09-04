8ff8bbacc9897c1968694762e9f4a5d9fc7b3965
annanwangdaniel <annanwangdaniel@outlook.com>
2022-09-04 20:25:47 +0800

    .

---
+++ job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH ‐‐partition=SCSEGPU_UG
+#SBATCH ‐‐qos=normal
+#SBATCH ‐‐nodes=1
+#SBATCH ‐‐ntasks‐per‐node=1
+#SBATCH ‐‐gres=gpu:1
+#SBATCH ‐‐mem=48G
+#SBATCH ‐‐job‐name=MyJob
+#SBATCH ‐‐output=output_%x_%j.out
+#SBATCH ‐‐error=error_%x_%j.err
+
+
+module load anaconda
+source activate coop
+bash scripts/coop/main.sh caltech101 rn50_ep50 end 16 1 False
