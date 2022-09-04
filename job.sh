1109ba6a858e34d1cd75073ffc6bf524b3425209
annanwangdaniel <annanwangdaniel@outlook.com>
2022-09-04 20:32:48 +0800

    .

--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 #SBATCH --partition=SCSEGPU_UG
 #SBATCH --qos=normal
 #SBATCH --nodes=1
-#SBATCH --ntasks‐per‐node=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=48G
-#SBATCH --job‐name=TestingJob
+#SBATCH --job-name=TestingJob
 #SBATCH --output=output_%x_%j.out
 #SBATCH --error=error_%x_%j.err
 
