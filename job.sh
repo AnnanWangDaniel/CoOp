551567ccbeb39d530dc0e881d4d4e3a8af793630
annanwangdaniel <annanwangdaniel@outlook.com>
2022-09-04 20:32:10 +0800

    .

--- job.sh
+++ job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --partition=SCSEGPU_UG
-#SBATCH ‐‐qos=normal
-#SBATCH ‐‐nodes=1
-#SBATCH ‐‐ntasks‐per‐node=1
-#SBATCH ‐‐gres=gpu:1
-#SBATCH ‐‐mem=48G
-#SBATCH ‐‐job‐name=TestingJob
-#SBATCH ‐‐output=output_%x_%j.out
-#SBATCH ‐‐error=error_%x_%j.err
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks‐per‐node=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=48G
+#SBATCH --job‐name=TestingJob
+#SBATCH --output=output_%x_%j.out
+#SBATCH --error=error_%x_%j.err
 
 
 module load anaconda
