6ca74d047dda73f8dbb672e44d0d63f4d03a669e
annanwangdaniel <annanwangdaniel@outlook.com>
2022-12-28 11:04:48 +0800

    .

--- train.py
+++ train.py
@@ -30,6 +30,7 @@ import trainers.clip_adapter
 import trainers.layer_norm
 import trainers.clip_prompt_adapter
 import trainers.layer_norm_adapter
+import trainers.coop_layer_norm
 
 
 def print_args(args, cfg):
